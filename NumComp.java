27b1fe5a4da65e1fd185089e9bc96a85ae8abb06
Ammiel <aemlcruz@gmail.com>
2017-05-25 00:05:02 -0400

    removed shifting operation

--- NumComp.java
+++ NumComp.java
@@ -4,8 +4,11 @@ public class NumComp {
     * flip the bits of its binary representation.
     */
     public int findComp(int num) {
-        int mask = (Integer.highestOneBit(num) << 1) - 1;
-        num = ~num; // perform a bitwise complement of num
-        return num & mask; // perform an AND bitwise operation on num and mask and return
+        // returns the left most highest one bit and subtract 1 to change the lowest bits
+        int mask = (Integer.highestOneBit(num)) - 1; 
+        // tilde(~) performs a bitwise complement of num
+        num = ~num; 
+        // perform an AND bitwise operation on num and mask and return
+        return num & mask; 
     }
 }
