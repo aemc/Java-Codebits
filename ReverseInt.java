1a0f97fe94a99059f6d22175567ad7045bd89d0b
Ammiel <aemlcruz@gmail.com>
2017-05-25 23:23:27 -0400

    sol to reverse integer

---
+++ ReverseInt.java
@@ -0,0 +1,18 @@
+public class ReverseInt {
+  /**
+  * Reverse digits of an integer
+  *
+  */
+  public int reverse(int x) {
+    int result = 0;
+    while (x != 0) {
+      int tail = x % 10;
+      int newResult = result * 10 + tail;
+      if ((newResult - tail)) / 10 != result) return 0;
+      result = newResult;
+      x = x / 10;
+    }
+    return result;
+  }
+
+}
